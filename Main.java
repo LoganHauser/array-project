f69bdb89f7e53a289283cb76d076d44f98c09acd
LaGz4643 <89419693+LoganHauser@users.noreply.github.com>
2022-10-07 11:18:23 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,16 +6,18 @@ public class Main {
         int[] numbers = new int[100];
         int sum = 0;
 
+        //Loop through the array and add numbers 1-100
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
         }
 
+        //Loop through array
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-            System.out.println(numbers[i] % 2 == 0 ? "even" : "odd");
-            sum += numbers[i];
+            System.out.print(numbers[i] + " "); //Print each number
+            System.out.println(numbers[i] % 2 == 0 ? "even" : "odd"); //Print whether the number is even or odd
+            sum += numbers[i]; //Add number to the sum variable
         }
 
-        System.out.println("sum: " + sum);
+        System.out.println("sum: " + sum); //Print total sum
     }
 }
